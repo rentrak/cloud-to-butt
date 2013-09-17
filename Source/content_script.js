74580282c141ab15db7d64c7fa3bfa19b6855e84
dag <dag@rentrak.com>
2013-09-16 22:05:25 -0700

    Changed from cloud to cliff and butt to rachel. Recompiled.

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,9 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bcliff\b/g, "rachel");
+	v = v.replace(/\bCliff\b/g, "Rachel");
+
 	
 	textNode.nodeValue = v;
 }
